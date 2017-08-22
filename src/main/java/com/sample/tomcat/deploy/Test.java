bea2f083c087dea167abc771e8fbbd13cbdbd39a
siva <sivamca.k6@gmail.com>
2017-08-23 00:36:29 +0530

    Changes.

--- src/main/java/com/sample/tomcat/deploy/Test.java
+++ src/main/java/com/sample/tomcat/deploy/Test.java
@@ -8,8 +8,18 @@ public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		 TomcatEmbedded.start("", "I:/git/embedded-tomcat-war-deploy/src/main/resources/ROOT.war");
+		TomcatEmbedded embedded = TomcatEmbedded.getInstance(8989,"", "I:/git/embedded-tomcat-war-deploy/src/main/resources/ROOT.war");
+		embedded.start();
+		
+		try {
+			Thread.currentThread().wait(10000l);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		embedded.stop();
 		 
 	}
 
+	
 }
--- src/main/java/com/sample/tomcat/deploy/TomcatEmbedded.java
+++ src/main/java/com/sample/tomcat/deploy/TomcatEmbedded.java
@@ -8,25 +8,33 @@ import org.apache.catalina.startup.Tomcat;
 import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainer;
 
 public class TomcatEmbedded extends TomcatEmbeddedServletContainer{
+	
+	private static TomcatEmbedded embedded;
+	
+	public static TomcatEmbedded getInstance(int port,String contextPath, String warLocation) {
+		if(embedded==null){
+			Tomcat tomcat = new Tomcat();
+			tomcat.setPort(8989);
+			new File(tomcat.getServer().getCatalinaBase(), "webapps").mkdirs();
+			tomcat.setBaseDir("I:/temp");
+			tomcat.getServer().setParentClassLoader(TomcatEmbedded.class.getClassLoader());
+			try {
+				tomcat.addWebapp(contextPath, warLocation);
+			} catch (ServletException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			embedded = new TomcatEmbedded(tomcat);
+		}
+		return embedded;
+	}
+	
+	
 
      public TomcatEmbedded(Tomcat tomcat) {
 		super(tomcat);
 		// TODO Auto-generated constructor stub
 	}
 
-	public static void start(String contextPath, String warLocation){
-		Tomcat tomcat = new Tomcat();
-		tomcat.setPort(8989);
-		//tomcat.addContext(contextPath,  new File(warLocation).getAbsolutePath());
-		new File(tomcat.getServer().getCatalinaBase(), "webapps").mkdirs();
-		tomcat.getServer().setParentClassLoader(TomcatEmbedded.class.getClassLoader());
-		try {
-			tomcat.addWebapp(contextPath, warLocation);
-		} catch (ServletException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		TomcatEmbedded embedded = new TomcatEmbedded(tomcat);
-		embedded.start();
-     }
+	 
 }
